453d3dd0afb140351b38e58c9ae14df9dcf395a3
Noah <noahkonen@yahoo.com>
2020-04-06 16:18:58 -0400

    OCR
    OCRV2 is the working project

---
+++ ICEZMath/src/OCRV2/OCRV2.java
@@ -0,0 +1,26 @@
+package OCRV2;
+
+
+import java.io.File;
+import net.sourceforge.tess4j.*;
+
+public class OCRV2 
+{
+    public static void main(String[] args) 
+    {
+        // ImageIO.scanForPlugins(); // for server environment
+        File imageFile = new File("D:\\JAVA Programing\\Eclipse\\Projects\\ICEZMath\\Test Pictures\\x.jpg");
+        ITesseract instance = new Tesseract(); // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+        instance.setDatapath("D:\\JAVA Programing\\Eclipse\\Projects\\ICEZMath\\Tesseract\\Tess4J\\tessdata"); // replace <parentPath> with path to parent directory of tessdata
+        instance.setLanguage("eng");
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) 
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# ICEZMath_Code
