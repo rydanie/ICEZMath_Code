f5b9bba14d09fcbf3c4caca8c8c7a0012391838d
Noah <noahkonen@yahoo.com>
2020-04-13 19:50:31 -0400

    Current Build
    4/13/2020

--- ICEZMath/src/OCRV2/OCRV2.java
+++ ICEZMath/src/OCRV2/OCRV2.java
@@ -1,21 +1,32 @@
 package OCRV2;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+
 import net.sourceforge.tess4j.*;
+
+
 
 public class OCRV2 
 {
-	public static String result;
+	static String result;
 	static StringManipulator sm;
+	static EquationSolver es;
+	static ArrayList eList;
+	
     public static void main(String[] args) 
     {
     	
+    	Random rand = new Random();
+    	File[] files = new File( "C:\\\\Users\\\\ryley\\\\OneDrive\\\\Documents\\\\GitHub\\\\ICEZMath_Code\\\\ICEZMath\\\\Test Pictures" ).listFiles();
+    	File file = files[ rand.nextInt( files.length ) ];
     	
         // ImageIO.scanForPlugins(); // for server environment
-        File imageFile = new File("D:\\JAVA Programing\\Eclipse\\Projects\\ICEZMath\\Test Pictures\\x.jpg");
+        File imageFile = file;//new File("C:\\Users\\ryley\\OneDrive\\Documents\\GitHub\\ICEZMath_Code\\ICEZMath\\Test Pictures\\x.jpg");
         ITesseract instance = new Tesseract(); // JNA Interface Mapping
         // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
-        instance.setDatapath("D:\\JAVA Programing\\Eclipse\\Projects\\ICEZMath\\Tesseract\\Tess4J\\tessdata"); // replace <parentPath> with path to parent directory of tessdata
+        instance.setDatapath("C:\\Users\\ryley\\OneDrive\\Documents\\GitHub\\ICEZMath_Code\\ICEZMath\\Tesseract\\Tess4J\\tessdata"); // replace <parentPath> with path to parent directory of tessdata
         instance.setLanguage("eng");
 
         try {
@@ -28,5 +39,8 @@ public class OCRV2
         
         
         sm = new StringManipulator(result);
+        eList = sm.getEquationList();
+        Equation zero = (Equation) eList.get(0);
+        es = new EquationSolver(zero);
     }
 }
